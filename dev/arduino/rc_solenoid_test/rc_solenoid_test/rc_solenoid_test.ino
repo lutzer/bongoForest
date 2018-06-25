3fe0c16e0d0dc9699b1fac8a3de36e99c983f872
lutzer <in_the_fishtank@gmx.de>
2018-06-25 11:18:56 +0200

    changed 3d models

---
+++ dev/arduino/rc_solenoid_test/rc_solenoid_test/rc_solenoid_test.ino
@@ -0,0 +1,49 @@
+#include <SPI.h>
+#include <NRFLite.h>
+
+#define SOLENOID_PIN 6
+#define LEDS_PIN 7
+
+#define RADIO_CE_PIN 8
+#define RADIO_CSN_PIN 10
+
+NRFLite _radio;
+uint8_t _data;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(SOLENOID_PIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LEDS_PIN, OUTPUT);
+
+  _radio.init(1, RADIO_CE_PIN, RADIO_CSN_PIN);
+
+  //test leds
+  digitalWrite(SOLENOID_PIN,HIGH);
+  delay(5000);
+  digitalWrite(SOLENOID_PIN,LOW);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+
+  while (_radio.hasData())
+  {
+    _radio.readData(&_data);
+  }
+
+   if ( _data == 111) {
+      digitalWrite(SOLENOID_PIN, HIGH);
+      digitalWrite(LED_BUILTIN,HIGH);
+      digitalWrite(LEDS_PIN,HIGH);
+      delay(30);                       
+      digitalWrite(SOLENOID_PIN, LOW);
+      digitalWrite(LED_BUILTIN,LOW);
+      digitalWrite(LEDS_PIN,LOW);
+      _data = 0;
+   }
+
+   
+   
+}
